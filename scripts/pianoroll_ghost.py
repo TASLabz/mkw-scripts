70b475e0517689a6564dc19fae822249bbfb3a32
xi <energeticcamel55@gmail.com>
2023-05-08 10:30:52 -0400

    Apply code review changes
    Co-Authored-By: Aiden <86704247+vabold@users.noreply.github.com>

--- scripts/pianoroll_ghost.py
+++ scripts/pianoroll_ghost.py
@@ -5,20 +5,18 @@ from framesequence import FrameSequence
 
 
 def set_ghost_buttons(inputs):
-    ghostcontrolleraddress = {"RMCE01": 0x809b8f70, "RMCP01": 0x809BD730,
-                              "RMCJ01": 0x809bc790, "RMCK01": 0x809abd70}
-    ghostcontrollerptr = core.chase_pointer(
-        ghostcontrolleraddress[core.get_game_id()], [0xC, 0x4, 0x48, 0x4], 'u32')
+    # NOTE: Ghost controller index 1 is consistent in the base game
+    ghost_controller = memory.read_u32(classes.InputMgr.chain()) + 0x3f08
 
-    buttons = memory.read_u32(ghostcontrollerptr + 0x94)
+    buttons = memory.read_u32(ghost_controller + 0x94)
     memory.write_u8(buttons + 0x12, inputs.accel + (inputs.brake << 1) +
                     (inputs.item << 2) | ((inputs.accel & inputs.brake) << 3))
 
-    stick = memory.read_u32(ghostcontrollerptr + 0x98)
+    stick = memory.read_u32(ghost_controller + 0x98)
     memory.write_u8(stick + 0x12, (inputs.stick_y + 7)
                     | ((inputs.stick_x + 7) << 4))
 
-    trickbuttons = memory.read_u32(ghostcontrollerptr + 0x9C)
+    trickbuttons = memory.read_u32(ghost_controller + 0x9C)
     if inputs.dpad_up:
         memory.write_u8(trickbuttons + 0x12, 0x1)
     elif inputs.dpad_down:
@@ -35,48 +33,69 @@ def set_ghost_buttons(inputs):
 def calc():
     global sequence
 
+    # NOTE: We're getting the Game ID here, but we only need the region from it.
+    region = utils.get_game_id()
+
     dbs_address = {"RMCE01": 0x8051c8d8, "RMCP01": 0x80520d4c,
                    "RMCJ01": 0x805206cc, "RMCK01": 0x8050ed70}
     fbs_address = {"RMCE01": 0x8051eacc, "RMCP01": 0x80522f40,
                    "RMCJ01": 0x805228c0, "RMCK01": 0x80510f64}
     tbs_address = {"RMCE01": 0x8051e7e8, "RMCP01": 0x80522c5c,
                    "RMCJ01": 0x805225dc, "RMCK01": 0x80510c80}
+
     inputs = sequence.frames[core.get_frame_of_input()]
-    if inputs and classes.RaceInfo.stage() >= 1:  # If there are inputs on this frame, send the inputs
-        # DirectionButtonsStream_readFrame
-        memory.write_u32(dbs_address[core.get_game_id()], 0x88630012) # lbz r3, 0x12 (r3)
-        memory.write_u32(
-            dbs_address[core.get_game_id()] + 0x4, 0x4e800020)        # blr
-        memory.invalidate_icache(dbs_address[core.get_game_id()], 0x8)
-        # FaceButtonsStream_readFrame
-        memory.write_u32(fbs_address[core.get_game_id()], 0x88630012) # lbz r3, 0x12 (r3)
-        memory.write_u32(
-            fbs_address[core.get_game_id()] + 0x4, 0x4e800020)        # blr
-        memory.invalidate_icache(fbs_address[core.get_game_id()], 0x8)
-        # TricksButtonStream_readFrame
-        memory.write_u32(tbs_address[core.get_game_id()], 0x88630012) # lbz r3, 0x12 (r3)
-        memory.write_u32(
-            tbs_address[core.get_game_id()] + 0x4, 0x4e800020)        # blr
-        memory.invalidate_icache(tbs_address[core.get_game_id()], 0x8)
-        set_ghost_buttons(inputs)
-    else:
-        # restore instructions if no inputs
+
+    if inputs and classes.RaceInfo.stage() >= 1:
+        # TODO: Handle on script enter
+        # If there are inputs on this frame, send the inputs
 
         # DirectionButtonsStream_readFrame
-        memory.write_u32(dbs_address[core.get_game_id()], 0x9421ffe0) # stwu sp, -0x20 (sp)
+        # lbz r3, 0x12 (r3)
+        # blr
+        memory.write_u32(dbs_address[region], 0x88630012)
         memory.write_u32(
-            dbs_address[core.get_game_id()] + 0x4, 0x7c0802a6)         # mflr r0
-        memory.invalidate_icache(dbs_address[core.get_game_id()], 0x8)
+            dbs_address[region] + 0x4, 0x4e800020)
+        memory.invalidate_icache(dbs_address[region], 0x8)
         # FaceButtonsStream_readFrame
-        memory.write_u32(fbs_address[core.get_game_id()], 0x9421ffe0) # stwu sp, -0x20 (sp)
+        # lbz r3, 0x12 (r3)
+        # blr
+        memory.write_u32(fbs_address[region], 0x88630012)
         memory.write_u32(
-            fbs_address[core.get_game_id()] + 0x4, 0x7c0802a6)        # mflr r0
-        memory.invalidate_icache(fbs_address[core.get_game_id()], 0x8)
+            fbs_address[region] + 0x4, 0x4e800020)
+        memory.invalidate_icache(fbs_address[region], 0x8)
         # TricksButtonStream_readFrame
-        memory.write_u32(tbs_address[core.get_game_id()], 0x9421ffe0) # stwu sp, -0x20 (sp)
+        # lbz r3, 0x12 (r3)
+        # blr
+        memory.write_u32(tbs_address[region], 0x88630012)
         memory.write_u32(
-            tbs_address[core.get_game_id()] + 0x4, 0x7c0802a6)        # mflr r0
-        memory.invalidate_icache(tbs_address[core.get_game_id()], 0x8)
+            tbs_address[region] + 0x4, 0x4e800020)
+        memory.invalidate_icache(tbs_address[region], 0x8)
+        set_ghost_buttons(inputs)
+    else:
+        # TODO: Handle on script exit
+        # Restore instructions if no inputs
+
+        # DirectionButtonsStream_readFrame
+        # stwu sp, -0x20 (sp)
+        # mflr r0
+        memory.write_u32(dbs_address[region], 0x9421ffe0)
+        memory.write_u32(
+            dbs_address[region] + 0x4, 0x7c0802a6)
+        memory.invalidate_icache(dbs_address[region], 0x8)
+        # FaceButtonsStream_readFrame
+        # stwu sp, -0x20 (sp)
+        # mflr r0
+        memory.write_u32(fbs_address[region], 0x9421ffe0)
+        memory.write_u32(
+            fbs_address[region] + 0x4, 0x7c0802a6)
+        memory.invalidate_icache(fbs_address[region], 0x8)
+        # TricksButtonStream_readFrame
+        # stwu sp, -0x20 (sp)
+        # mflr r0
+        memory.write_u32(tbs_address[region], 0x9421ffe0)
+        memory.write_u32(
+            tbs_address[region] + 0x4, 0x7c0802a6)
+        memory.invalidate_icache(tbs_address[region], 0x8)
 
 
 @event.on_savestateload
