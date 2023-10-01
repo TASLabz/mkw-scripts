27df090f2160840e05e368a35ae511a06927dc39
sowens99 <seanowens@comcast.net>
2023-10-01 14:54:47 -0400

    Use classes to get inputs

--- scripts/_draw_input_display.py
+++ scripts/_draw_input_display.py
@@ -1,7 +1,6 @@
 from dolphin import gui, event
-import input_display as display
-import mkw_classes as classes
-import mkw_core as core
+from Modules import input_display as display
+from Modules import mkw_classes as classes
 
 stick_dict = {-7: 0, -6: 60, -5: 70, -4: 80, -3: 90, -2: 100, -1: 110,
               0: 128, 1: 155, 2: 165, 3: 175, 4: 185, 5: 195, 6: 200, 7: 255}
@@ -11,20 +10,12 @@ stick_dict = {-7: 0, -6: 60, -5: 70, -4: 80, -3: 90, -2: 100, -1: 110,
 def on_frame_advance():
     if classes.RaceInfo.stage() >= 1:
 
-        # TODO: change to functions in classes module when rewritten
         # TODO: use masks instead of the values for buttons
 
-        ablr = core.chase_pointer(classes.getRaceInfoHolder(), [
-            0xC, 0x0, 0x48, 0x4, 0x9], 'u8')
-
-        dpad = core.chase_pointer(
-            classes.getRaceInfoHolder(), [0xC, 0x0, 0x48, 0x4, 0x17], 'u8')
-
-        xstick = core.chase_pointer(
-            classes.getRaceInfoHolder(), [0xC, 0x0, 0x48, 0x38], 'u8') - 7
-
-        ystick = core.chase_pointer(
-            classes.getRaceInfoHolder(), [0xC, 0x0, 0x48, 0x39], 'u8') - 7
+        ablr = classes.RaceInfoPlayer.ablr_buttons()
+        dpad = classes.RaceInfoPlayer.dpad()
+        xstick = classes.RaceInfoPlayer.stick_x() - 7
+        ystick = classes.RaceInfoPlayer.stick_y() - 7
 
         # A Button
         display.create_unpressed_button([330, gui.get_display_size()[1] - 95],
@@ -66,5 +57,5 @@ def on_frame_advance():
                               30, -35, 0xFFFFFFFF, ["Right"])
 
         # Control Stick
-        display.create_control_stick([210, gui.get_display_size()[
-            1] - 100], 50, 30, 50, stick_dict.get(xstick, 0), stick_dict.get(ystick, 0), 0xFFFFFFFF)
+        display.create_control_stick([210, gui.get_display_size()[1] - 100], 50, 30, 50,
+            stick_dict.get(xstick, 0), stick_dict.get(ystick, 0), 0xFFFFFFFF)
